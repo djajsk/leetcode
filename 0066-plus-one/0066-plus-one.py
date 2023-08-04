4f5b482355f05ce6ba10671d6987c06a8024575e
Dhruvil Jain <92313597+djajsk@users.noreply.github.com>
2023-08-04 13:54:58 +0530

    Time: 49 ms (50.71%), Space: 16.2 MB (92.98%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+      integer = 0
+      for i in digits:
+        integer = integer*10 + i
+      integer += 1
+      res = []
+      while integer != 0:
+        res.insert(0, integer % 10)
+        integer = integer//10
+      return res
